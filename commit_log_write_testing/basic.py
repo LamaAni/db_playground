c938a3e09f2f9078146b33d9e3389c483454889c
zav <zav.shotan@gmail.com>
2024-05-01 14:43:37 -0400

    updated

--- commit_log_write_testing/basic.py
+++ commit_log_write_testing/basic.py
@@ -6,12 +6,22 @@ from zthreading.tasks import Task
 
 
 def test_writer():
+    logical_clock = 0
+    sep = " ".encode("utf-8")
+    lnend = "\n".encode("utf-8")
     with open(
-        os.path.join(REPO_LOCAL_PATH, "test_writer.dump"), "w", encoding="utf-8"
+        os.path.join(REPO_LOCAL_PATH, "test_writer.dump"),
+        mode="ab",
+        buffering=10000,
     ) as src:
         while True:
-            src.write(str(random.random()))
-            src.write("\n")
+            logical_clock += 1
+            src.write(bytes(logical_clock))
+            src.write(sep)
+            src.write(str(random.random()).encode(encoding="utf-8"))
+            src.write(lnend)
+            if logical_clock == 1e10:
+                break
 
 
 def test_halter():

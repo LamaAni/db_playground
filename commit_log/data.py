2d94c18178ca60d20f2dd33c3d72272e5f19c23d
zav <zav.shotan@gmail.com>
2024-05-17 12:21:55 -0400

    updated

--- commit_log/data.py
+++ commit_log/data.py
@@ -4,11 +4,34 @@
 # data model.
 # Each entry in the class can use a serializer/deserializer
 # that is configurable.
+import json
 
 
 class EntrySerializer:
+    def serialize(entry: "Entry", encoding: str = "utf-8") -> bytes:
+        return json.dumps(
+            dict(
+                lc=entry.logical_clock,
+                md=entry.metadata,
+                d=entry.data,
+            )
+        ).encode(encoding=encoding)
 
-    pass
+    def deserialize(entry: bytes | str, encoding: str = "utf-8"):
+        if isinstance(entry, bytes):
+            entry = entry.decode(encoding=encoding)
+        entry_dict = json.loads(entry)
+        return Entry(
+            logical_clock=entry_dict["lc"],
+            metadata=entry_dict.get("md"),
+            data=entry_dict.get("d"),
+        )
+
+
+DEFAULT_SERIALIZER: EntrySerializer = EntrySerializer()
+"""
+The default serializer used when serializing an entry.
+"""
 
 
 class Entry:
@@ -16,11 +39,23 @@ class Entry:
     class and holds information about an entry.
     """
 
-    DEFAULT_SERIALIZER: EntrySerializer = EntrySerializer()
-    """
-    The default serializer used when serializing an entry.
-    This would be replaced with
-    """
+    def __init__(
+        self,
+        logical_clock: int = None,
+        metadata: dict = None,
+        data: bytes | str = None,
+    ) -> None:
+        """Creates a new data entry
 
-    def __init__(self, serializer: EntrySerializer = None) -> None:
-        pass
+        Args:
+            logical_clock (int, optional): The logical clock (index) of the data entry, within a log file.
+                Denotes the order of the logs in the file.
+                Defaults to None.
+            metadata (dict, optional): The log metadata as dictionary.
+                May include timestamp, type .. etc. JSON dictionary values only.
+                Defaults to None.
+            data (bytes | str | dict, optional): The log entry data. Defaults to None.
+        """
+        self.logical_clock = logical_clock
+        self.metadata = metadata
+        self.data = data
